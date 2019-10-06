f413b17caf26cc5cd7a7e133738bca64b2515e6b
ClockMoon <jiwon3346@naver.com>
2019-10-06 17:14:13 +0900

    ch1 build a schema

--- server/Tutorials/src/index.ts
+++ server/Tutorials/src/index.ts
@@ -1,6 +1,9 @@
 import { ApolloServer } from 'apollo-server'
+import typeDefs from './schema'
 
-const server = new ApolloServer({})
+const server = new ApolloServer({
+  typeDefs,
+})
 
 server.listen().then(({ url }: any) => {
   console.log(`🚀 Server ready at ${url}`)
