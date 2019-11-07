6aaefc00c91b74b408e6dcc4ae3364aeff7c3eea
ClockMoon <jiwon3346@naver.com>
2019-11-07 16:25:02 +0900

    interface

--- server/Tutorials/AdvancedApolloServer/union.js
+++ server/Tutorials/AdvancedApolloServer/union.js
@@ -3,9 +3,26 @@ const { gql, ApolloServer } = require('apollo-server')
 const typeDefs = gql`
   union Result = Book | Author
 
+  interface BookInterface {
+    title: String
+    author: String
+  }
+
   type Book {
     title: String
     sale: Boolean
+  }
+
+  type TextBook implements BookInterface {
+    title: String
+    author: String
+    classes: String
+  }
+
+  type ColoringBook implements BookInterface {
+    title: String
+    author: String
+    colors: String
   }
 
   type Author {
@@ -15,10 +32,24 @@ const typeDefs = gql`
 
   type Query {
     search: [Result]
+    schoolBooks: [BookInterface]
   }
 `
 
 const resolvers = {
+  BookInterface: {
+    __resolveType(book, context, info) {
+      if (book.classes) {
+        return 'TextBook'
+      }
+
+      if (book.colors) {
+        return 'ColoringBook'
+      }
+
+      return null
+    },
+  },
   Result: {
     __resolveType(obj, context, info) {
       if (obj.name) {
@@ -33,6 +64,7 @@ const resolvers = {
   },
   Query: {
     search: () => [{ name: 'Yes I am Author', age: 22 }, { title: 'Yes My Book', sale: false }],
+    schoolBooks: () => [{ title: 'colorTitle', author: 'colorAuthor', colors: 'RED' }],
   },
 }
 
