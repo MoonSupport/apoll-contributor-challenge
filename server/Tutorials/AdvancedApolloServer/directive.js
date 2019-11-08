4eeeaf346260767b5b4f4ada260baab17afb1413
ClockMoon <jiwon3346@naver.com>
2019-11-08 22:49:33 +0900

    directive

---
+++ server/Tutorials/AdvancedApolloServer/directive.js
@@ -0,0 +1,47 @@
+const { ApolloServer, gql, SchemaDirectiveVisitor } = require('apollo-server')
+const { defaultFieldResolver } = require('graphql')
+
+// Create (or import) a custom schema directive
+class UpperCaseDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const { resolve = defaultFieldResolver } = field
+    field.resolve = async function(...args) {
+      const result = await resolve.apply(this, args)
+      if (typeof result === 'string') {
+        return result.toUpperCase()
+      }
+      return result
+    }
+  }
+}
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  directive @upper on FIELD_DEFINITION
+
+  type Query {
+    hello: String @upper
+  }
+`
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (parent, args, context) => {
+      return 'Hello world!'
+    },
+  },
+}
+
+// Add directive to the ApolloServer constructor
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  schemaDirectives: {
+    upper: UpperCaseDirective,
+  },
+})
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
---
+++ server/Tutorials/AdvancedApolloServer/scalar.js
@@ -0,0 +1,78 @@
+const { GraphQLScalarType } = require('graphql')
+const { Kind } = require('graphql/language')
+const { gql, ApolloServer } = require('apollo-server')
+
+const typeDefs = gql`
+  scalar Odd
+
+  enum AllowedColor {
+    RED
+    GREEN
+    BLUE
+  }
+
+  type MyType {
+    oddValue: Odd
+  }
+
+  type Query {
+    myType: MyType
+    favoriteColor: AllowedColor # As a return value
+    avatar(borderColor: AllowedColor): String # As an argument
+  }
+`
+var cursor = 1
+
+function oddValue(value) {
+  console.log(value)
+  return value % 2 === 1 ? value : null
+}
+
+const resolvers = {
+  Odd: new GraphQLScalarType({
+    name: 'Odd',
+    description: 'Odd custom scalar type',
+    parseValue: oddValue,
+    serialize: oddValue,
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return oddValue(parseInt(ast.value, 10))
+      }
+      return null
+    },
+  }),
+
+  AllowedColor: {
+    RED: '#f00',
+    GREEN: '#0f0',
+    BLUE: '#00f',
+  },
+
+  Query: {
+    myType: () => {
+      return {
+        oddValue: 1,
+      }
+    },
+    favoriteColor: () => '#f00',
+    avatar: (parent, args) => {
+      console.log(parent)
+      if (args.borderColor == 'RED') {
+        return 'HOT'
+      }
+      if (args.borderColor == 'GREEN') {
+        return 'RELAX'
+      }
+      if (args.borderColor == 'BLUE') {
+        return 'COOL'
+      }
+      return 'NO!'
+    },
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
