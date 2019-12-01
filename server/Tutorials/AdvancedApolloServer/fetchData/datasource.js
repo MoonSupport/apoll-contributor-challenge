1d6afddea31e30d726817caad0faf1d4ae6b5ddd
ClockMoon <jiwon3346@naver.com>
2019-12-01 22:44:38 +0900

    fetch REST

--- server/Tutorials/AdvancedApolloServer/fetchData/datasource.js
+++ server/Tutorials/AdvancedApolloServer/fetchData/datasource.js
@@ -1,43 +1,43 @@
-// const { RESTDataSource } = require('apollo-datasource-rest')
+const { RESTDataSource } = require('apollo-datasource-rest')
 
-// class MoviesAPI extends RESTDataSource {
-//   constructor() {
-//     super()
-//     this.baseURL = 'https://movies-api.example.com/'
-//   }
+class MoviesAPI extends RESTDataSource {
+  constructor() {
+    super()
+    this.baseURL = 'https://movies-api.example.com/'
+  }
 
-//   async getMovie(id) {
-//     return this.get(`movies/${id}`)
-//   }
+  async getMovie(id) {
+    return this.get(`movies/${id}`)
+  }
 
-//   async getMostViewedMovies(limit = 10) {
-//     const data = await this.get('movies', {
-//       per_page: limit,
-//       order_by: 'most_viewed',
-//     })
-//     return data.results
-//   }
-// }
+  async getMostViewedMovies(limit = 10) {
+    const data = await this.get('movies', {
+      per_page: limit,
+      order_by: 'most_viewed',
+    })
+    return data.results
+  }
+}
 
-// class PersonalizationAPI extends RESTDataSource {
-//     constructor() {
-//       super();
-//       this.baseURL = 'https://personalization-api.example.com/';
-//     }
+class PersonalizationAPI extends RESTDataSource {
+    constructor() {
+      super();
+      this.baseURL = 'https://personalization-api.example.com/';
+    }
 
-//     willSendRequest(request) {
-//       request.headers.set('Authorization', this.context.token);
-//     }
+    willSendRequest(request) {
+      request.headers.set('Authorization', this.context.token);
+    }
 
-//     private progressLoader = new DataLoader(async (ids) => {
-//       const progressList = await this.get('progress', {
-//         ids: ids.join(','),
-//       });
-//       return ids.map(id =>
-//         progressList.find((progress) => progress.id === id),
-//       );
-//     });
+    private progressLoader = new DataLoader(async (ids) => {
+      const progressList = await this.get('progress', {
+        ids: ids.join(','),
+      });
+      return ids.map(id =>
+        progressList.find((progress) => progress.id === id),
+      );
+    });
 
-//     async getProgressFor(id) {
-//       return this.progressLoader.load(id);
-//     }
+    async getProgressFor(id) {
+      return this.progressLoader.load(id);
+    }
